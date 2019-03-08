f35b69fae6bcb1ca689d4f231376c871d7534cc8
Pierre Casco <pierre@Pierres-MBP.home>
2019-03-07 20:05:02 -0500

    2nd commit

--- Pierre_Casco_HW9.R
+++ Pierre_Casco_HW9.R
@@ -1,17 +1,20 @@
 library('arules')
 library('kernlab')
-library('dplyr')
 
 #Load the air quality dataset
-aq <- na.omit(airquality)
+aq <- airquality
+aq[is.na(aq)] <- 0
 
 #Study data set
 str(aq)
 
 #Prepare train and test data sets
-numrows <- nrow(aq)
+numrows <- nrow(aqt)
 cutoff <- (numrows/3*2)
 randIndex <- sample(1:numrows[1])
 
 aq.train <- aq[randIndex[1:cutoff],]
 aq.test <- aq[randIndex[(cutoff+1):numrows],]
+
+#Build model using KSVM
+model <- ksvm(Ozone ~ Solar.R + Wind + Temp, data = aq.train, kernel = "rbfdot", kpar = "automatic", C = 5, cross = 3, prob.model = TRUE)
