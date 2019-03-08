62f9e753631e4f831782de1a45724403a291f7b8
Pierre Casco <pierre@Pierres-MBP.home>
2019-03-07 21:09:33 -0500

    3rd commit

--- Pierre_Casco_HW9.R
+++ Pierre_Casco_HW9.R
@@ -8,6 +8,8 @@ aq[is.na(aq)] <- 0
 #Study data set
 str(aq)
 
+aq$Ozone <- as.factor(aq$Ozone)
+
 #Prepare train and test data sets
 numrows <- nrow(aqt)
 cutoff <- (numrows/3*2)
@@ -17,4 +19,13 @@ aq.train <- aq[randIndex[1:cutoff],]
 aq.test <- aq[randIndex[(cutoff+1):numrows],]
 
 #Build model using KSVM
-model <- ksvm(Ozone ~ Solar.R + Wind + Temp, data = aq.train, kernel = "rbfdot", kpar = "automatic", C = 5, cross = 3, prob.model = TRUE)
+model <- ksvm(Ozone ~ Solar.R + Wind + Temp, data = aq.train,kernel = "rbfdot", kpar = "automatic", C = 20)
+
+#Test the model on the testing dataset and compute RMSE
+svmPred <- predict(model, aq.test, type="votes")
+RMSE <- sqrt(mean(aq.test$Ozone - svmPred)^2)
+
+#Plot
+ggplot(aq.test,aes(x=Temp,y=Wind)) + 
+  geom_point(aes(),size=(aq.test$Ozone - svmPred), colour = (abs(svmPred - aq.test$Ozone))) + 
+  scale_color_manual(values = abs(svmPred - aq.test$Ozone))
